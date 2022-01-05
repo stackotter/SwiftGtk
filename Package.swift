37ac0d419b828fc979eecf753dced065d0b18bcf
stackotter <stackotter@stackotter.dev>
2022-01-06 08:41:58 +1000

    Make CGtk platform specific again (but neater than before)

--- Package.swift
+++ Package.swift
@@ -2,11 +2,20 @@
 
 import PackageDescription
 
+#if os(OSX)
+  let cGtkSources = "Sources/CGtk/MacOS"
+#elseif os(Linux)
+  let cGtkSources = "Sources/CGtk/Linux"
+#else
+  fatalError("Unsupported platform.")
+#endif
+
 let package = Package(
   name:  "SwiftGtk",
   targets: [
     .systemLibrary(
-      name: "CGtk", 
+      name: "CGtk",
+      path: cGtkSources,
       pkgConfig: "gtk+-3.0", 
       providers: [.brew(["gtk+3"]), 
       .apt(["libgtk-3-dev clang"])]),
