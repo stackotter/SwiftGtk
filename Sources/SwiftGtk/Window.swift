28ce7bb78e53cfd071b65a3e2c32f48a5a4f958c
Jorge Ramirez <jorge.a.ramirez@mac.com>
2022-04-26 23:50:36 -0400

    Cleaned up Window

--- Sources/SwiftGtk/Window.swift
+++ Sources/SwiftGtk/Window.swift
@@ -5,19 +5,6 @@
 import CGtk
 
 public class Window: Bin {
-    public enum WindowType {
-        case topLevel
-        case popUp
-
-        fileprivate func toGtkWindowType() -> GtkWindowType {
-            switch self {
-            case .topLevel:
-                return GTK_WINDOW_TOPLEVEL
-            case .popUp:
-                return GTK_WINDOW_POPUP
-            }
-        }
-    }
 
     public init(windowType: WindowType = .topLevel) {
         super.init()
@@ -25,38 +12,26 @@ public class Window: Bin {
     }
 
     public var title: String? {
-        get {
-            return String(cString: gtk_window_get_title(castedPointer()))
-        }
-        set {
-            if let title = newValue {
-                gtk_window_set_title(castedPointer(), title)
-            } else {
-                gtk_window_set_title(castedPointer(), nil)
-            }
-        }
+        get { String(cString: gtk_window_get_title(castedPointer())) }
+        set { gtk_window_set_title(castedPointer(), newValue) }
     }
 
     public var defaultSize: Size {
         get {
-            var width: Int32 = 0
-            var height: Int32 = 0
+            var width: gint = 0
+            var height: gint = 0
             gtk_window_get_default_size(castedPointer(), &width, &height)
 
             return Size(width: Int(width), height: Int(height))
         }
         set (size) {
-            gtk_window_set_default_size(castedPointer(), Int32(size.width), Int32(size.height))
+            gtk_window_set_default_size(castedPointer(), gint(size.width), gint(size.height))
         }
     }
 
     public var resizable: Bool {
-        get {
-            return gtk_window_get_resizable(castedPointer()).toBool()
-        }
-        set {
-            gtk_window_set_resizable(castedPointer(), newValue.toGBoolean())
-        }
+        get { gtk_window_get_resizable(castedPointer()).toBool() }
+        set { gtk_window_set_resizable(castedPointer(), newValue.toGBoolean()) }
     }
 
     public var hideTitlebarWhenMaximized: Bool {
@@ -66,7 +41,7 @@ public class Window: Bin {
 
     private var _titleBar: Widget?
     public var titlebar: Widget? {
-        get { return _titleBar }
+        get { _titleBar }
         set { gtk_window_set_titlebar(castedPointer(), newValue?.widgetPointer) }
     }
 }
